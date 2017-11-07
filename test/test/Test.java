19876a27adf1f6e5d3d7234fc34111d580badd90
Linus Dierheimer <Linus@Dierheimer.de>
2017-11-07 20:21:47 +0100

    tests

--- test/test/Test.java
+++ test/test/Test.java
@@ -1,35 +1,9 @@
 package test;
 
-import javafx.scene.paint.Paint;
-import linus.io.config.Config;
-import linus.io.config.configs.SingleStringConfig;
-import linus.io.config.io.ComplexConfigReader;
-import linus.io.config.io.ComplexConfigReaderBase;
-
 public class Test {
 
 	private Test() {}
 
 	public static void main(String[] args) throws Exception{
-		Config<Paint> ownConfig = new Config<Paint>("Name", Paint.valueOf("blue")) {
-			
-			
-			@Override
-			public String[] write() {
-				return new String[] { getName() + " = " + getValueAsString()};
-			}
-			
-			@Override
-			public Config<Paint> read(String[] lines) {
-				SingleStringConfig ssc = (SingleStringConfig) new SingleStringConfig().read(lines);
-				this.name = ssc.getName();
-				this.value = Paint.valueOf(ssc.getValue());
-				return this;
-			}
-		};
-		ComplexConfigReaderBase ccr = new ComplexConfigReader(".\\config.cfg");
-		Config<?> cfg = ccr.next();
-		System.out.println(cfg);
-		ccr.close();
 	}
 }
